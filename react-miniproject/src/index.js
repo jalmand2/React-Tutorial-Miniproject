beba9d9762c15943ed409da9b4587db4e0157bbf
Jessica Almand <jlalmand27@gmail.com>
2023-11-24 13:15:08 -0700

    Edited starting code and deleted unnecessary files

---
+++ react-miniproject/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './App';
+
+// This is our starting point of the react application
+// The first code to be executed in the browser
+ReactDOM.render(<App />, document.getElementById('root'));
+   
