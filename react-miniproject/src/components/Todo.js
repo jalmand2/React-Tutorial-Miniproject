5fe44aee959b65fe158938c4bd6171e1ea80b1e0
Jessica Almand <jlalmand27@gmail.com>
2023-11-27 11:45:31 -0700

    Added useState to Todo.js, creating function to show modal when button is clicked to delete

--- react-miniproject/src/components/Todo.js
+++ react-miniproject/src/components/Todo.js
@@ -1,6 +1,16 @@
+import { useState } from 'react';
+// useState is a react hook and can only be called directly in react component functions and in custom hooks
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+
 function Todo(props) {
+    const [ showModal, setModalIsOpen ] = useState(false);
+    //stored value in first element above, second element is a function that allows you to change the stored value
+    //useState returns an array with two elements
     function deleteHandler() {
-        
+        setModalIsOpen(true);
     } 
   return (
   <div className="card">
@@ -8,6 +18,8 @@ function Todo(props) {
     <div classsName="actions">
       <button className="btn" onClick={deleteHandler}>Delete</button>
     </div>
+    { showModal && <Modal /> }
+    { showModal && <Backdrop /> }
   </div>
   );
 }
